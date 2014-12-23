b928d801449766776633aeeadbfb67b38d0718db
mknadler <kennedy.nadler@gmail.com>
2014-12-23 12:55:33 -0500

    Step 4 - tasks/forms

--- simple-todos.js
+++ simple-todos.js
@@ -2,9 +2,31 @@ Tasks = new Mongo.Collection("tasks");
 
 if (Meteor.isClient) {
   // code only runs on client
+
+// template w/ helper
   Template.body.helpers({
   	tasks: function () {
-  		return Tasks.find({});
+  		// sort by most recent at top
+  		return Tasks.find({}, {sort: {createdAt: -1}});
   	}
   });
+
+  // template w/ event listener
+  Template.body.events({
+  	// listen for 'submit' event on any element that matches .new-task
+  	"submit .new-task": function (event) {
+  		var text = event.target.text.value;
+  		// event.target = form element; .text.value = input val
+  		Tasks.insert({
+  			text: text,
+  			createdAt: new Date() // current time
+  		});
+
+  		// Clear form -- make input blank after task inserted into Tasks collection
+  		event.target.text.value = "";
+
+  		// Prevent default form submission
+  		return false;
+  	}
+  })
 }
